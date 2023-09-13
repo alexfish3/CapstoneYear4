fc785a0b02f0d57774bf25adf244fed04f5e8474
Alex <alexander052702@gmail.com>
2023-09-13 15:59:43 -0400

    added player movement

--- Assets/Scripts/Alex/ScooterMovement.cs
+++ Assets/Scripts/Alex/ScooterMovement.cs
@@ -10,11 +10,17 @@ public class ScooterMovement : MonoBehaviour
     public Vector2 playerMovement;
 
     [SerializeField] float accelerationValue;
-    [SerializeField] float speed;
+    [SerializeField] float acceleration;
     [SerializeField] float maxSpeed;
 
+    [Header("Camera Values")]
+    public GameObject cameraHolder;
+    public float rotationControl;
+
     [Header("Steer Values")]
-    public float steerControl;
+    float steerControl;
+    public float turningValue;
+    public GameObject wheel;
 
     [Header("Accelerating Info")]
     public bool accelerating;
@@ -22,6 +28,9 @@ public class ScooterMovement : MonoBehaviour
     [Header("Braking Info")]
     public bool braking;
 
+    [Header("Boost Info")]
+    public int boostCount;
+    public bool boosting;
 
     Rigidbody rb;
 
@@ -32,17 +41,30 @@ public class ScooterMovement : MonoBehaviour
 
     private void Update()
     {
+        //Vector3 cameraRotation = new Vector3(cameraHolder.transform.rotation.x, cameraHolder.transform.rotation.y + rotationControl, cameraHolder.transform.rotation.z);
+        //cameraHolder.transform.rotation = Quaternion.Euler(cameraRotation);
+
         PlayerMove();
     }
 
     private void PlayerMove()
     {
+        gameObject.transform.rotation *= Quaternion.AngleAxis((steerControl * turningValue), Vector3.up);
 
+
+        // Moving Forward
         if (accelerating && rb.velocity.magnitude <= maxSpeed)
         {
-            Vector3 playerTransform = new Vector3(playerMovement.x, 0, playerMovement.y);
-            rb.AddForce(playerTransform * speed, ForceMode.Acceleration);
+            //Vector3 playerTransform = new Vector3(playerMovement.x, 0, playerMovement.y);
+            rb.AddForce(transform.forward * acceleration, ForceMode.Acceleration);
         }
+
+        // Boost Forward
+        if (accelerating && boostCount > 0 && boosting)
+        {
+            rb.AddForce(transform.forward * 2, ForceMode.Impulse);
+        }
+
     }
 
     public void OnMoveController(CallbackContext context)
@@ -53,6 +75,10 @@ public class ScooterMovement : MonoBehaviour
     public void OnSteer(CallbackContext context)
     {
         steerControl = context.ReadValue<float>();
+    }
+    public void OnCameraRotation(CallbackContext context)
+    {
+        rotationControl = context.ReadValue<float>();
     }
 
     public void OnAccelerate(CallbackContext context)
@@ -79,5 +105,16 @@ public class ScooterMovement : MonoBehaviour
         }
     }
 
+    public void OnBoost(CallbackContext context)
+    {
+        if (context.performed)
+        {
+            boosting = true;
+        }
+        else if (context.canceled)
+        {
+            boosting = false;
+        }
+    }
 
 }
