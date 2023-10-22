d14b464e5a0e63e0bc7d66c89903e0680e24fccd
Alex <alexander052702@gmail.com>
2023-10-21 21:27:57 -0400

    Implemented simple player loader
    Added player loader/ready-up. Also updated singleton monobehaviour to include option for don't destroy on load

--- Assets/Scripts/Management/SingletonMonobehaviour.cs
+++ Assets/Scripts/Management/SingletonMonobehaviour.cs
@@ -1,8 +1,14 @@
+/* Script Created by: Alex Fischer
+ * Year: 2023
+ * Purpose: To act as a universal singleton option to derrive from. To be used in any project 
+ */
 using UnityEngine;
 
 public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+    [Space(10)]
+    public bool dontDestroyOnLoad;
 
     public static T Instance
     {
@@ -11,13 +17,27 @@ public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBe
 
     protected virtual void Awake()
     {
-        if(instance == null)
+        // Normal singleton reference
+        if (instance == null)
         {
             instance = this as T;
         }
         else
         {
             Destroy(gameObject);
+        }
+
+        // If true, allow for scene loading
+        if (dontDestroyOnLoad)
+        {
+            T[] objs = FindObjectsOfType<T>();
+            if (objs.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+
+
+            DontDestroyOnLoad(this.gameObject);
         }
     }
 }
--- Assets/Scripts/Player/PlayerInstantiate.cs
+++ Assets/Scripts/Player/PlayerInstantiate.cs
@@ -1,22 +1,126 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-public class PlayerInstantiate : MonoBehaviour
+public class PlayerInstantiate : SingletonMonobehaviour<PlayerInstantiate>
 {
-    [SerializeField] 
+    const int MAX_PLAYERS = 4;
+    const int MAX_REQUIRED_READY_UP = 2;
 
+    [Header("Player References")]
+    [SerializeField] int playerCount = 0;
+    [SerializeField] PlayerInput[] avaliblePlayerInputs = new PlayerInput[MAX_PLAYERS];
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Ready Up Information")]
+    [SerializeField] bool[] playerReadyUp = new bool[MAX_PLAYERS];
+    public static UnityEvent OnReadiedUp;
+    int readyUpCounter = 0;
+
+    ///<summary>
+    /// Event to add the player references and rename player
+    ///</summary>
+    public void AddPlayerReference(PlayerInput playerInput)
     {
-        
+        playerCount++;
+
+        playerInput.gameObject.name = "Player " + playerCount.ToString();
+
+        Debug.Log("Spawned Player " + playerCount);
+        AddToPlayerArray(playerInput);
+
+        // Temp, remove once we enable input ready up
+        PlayerReady(playerInput);
     }
 
-    // Update is called once per frame
-    void Update()
+    ///<summary>
+    /// Event to remove the player from references
+    ///</summary>
+    public void RemovePlayerReference(PlayerInput playerInput)
     {
-        
+        playerCount--;
     }
+
+    ///<summary>
+    /// Adds the player input to the player array
+    ///</summary>
+    public void AddToPlayerArray(PlayerInput playerInput)
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (avaliblePlayerInputs[i] == null)
+            {
+                avaliblePlayerInputs[i] = playerInput;
+                return;
+            }
+        }
+    }
+
+    ///<summary>
+    /// Removes the player input from the player array
+    ///</summary>
+    public void RemoveFromPlayerArray(PlayerInput playerInput)
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (avaliblePlayerInputs[i] == playerInput)
+            {
+                avaliblePlayerInputs[i] = null;
+                return;
+            }
+        }
+    }
+
+    ///<summary>
+    /// Allows Player To Ready
+    ///</summary>
+    public void PlayerReady(PlayerInput playerInput)
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (avaliblePlayerInputs[i] == playerInput)
+            {
+                playerReadyUp[i] = true;
+                return;
+            }
+        }
+
+        // After readying up player, check if you can start game
+        CheckReadyUpCount();
+    }
+
+    ///<summary>
+    /// Allows Player To Not Ready
+    ///</summary>
+    public void PlayerNotReady(PlayerInput playerInput)
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (avaliblePlayerInputs[i] == playerInput)
+            {
+                playerReadyUp[i] = false;
+                return;
+            }
+        }
+    }
+
+    ///<summary>
+    /// Checks for how many players are readied up
+    ///</summary>
+    public void CheckReadyUpCount()
+    {
+        // Resets and loops to count amount of ready ups
+        readyUpCounter = 0;
+        foreach (bool readyUpValue in playerReadyUp)
+        {
+            if (readyUpValue)
+                readyUpCounter++;
+        }
+
+        // Checks if number of readied up is greater then or equal to required amount
+        if(readyUpCounter >= MAX_REQUIRED_READY_UP)
+        {
+            OnReadiedUp?.Invoke();
+        }
+    }
+
 }
