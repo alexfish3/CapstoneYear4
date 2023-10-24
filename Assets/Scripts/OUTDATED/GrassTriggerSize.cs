2364623399182f5e0e5564a765602e7a62b4a0c9
LoganM5 <loganmmoss@gmail.com>
2023-10-24 01:52:40 -0400

    whitebox area

--- Assets/Scripts/OUTDATED/GrassTriggerSize.cs
+++ Assets/Scripts/OUTDATED/GrassTriggerSize.cs
@@ -7,7 +7,7 @@ public class GrassTriggerSize : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<BoxCollider>().size = new Vector3 (10 - gameObject.transform.localScale.z, 3, 10 - gameObject.transform.localScale.x);
+        gameObject.GetComponent<BoxCollider>().size = new Vector3 (10 - gameObject.transform.localScale.z * 1.5f, 3, 10 - gameObject.transform.localScale.x * 1.5f);
     }
 
 }
