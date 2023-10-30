d9c58b804acacd50573c44d9a9bf4cd2ac721d7d
LoganM5 <loganmmoss@gmail.com>
2023-10-30 01:17:27 -0400

    fuck it we ball (chatgpt did all the work)

--- Assets/Scripts/OUTDATED/Respawn.cs
+++ Assets/Scripts/OUTDATED/Respawn.cs
@@ -6,16 +6,21 @@ using UnityEngine.UIElements;
 public class Respawn : MonoBehaviour
 {
     //Variables
-    Vector3 respawnPoint;
+    private Vector3 respawnPoint;
+    private Quaternion initialRotation;
 
+    public float liftHeight = 5.0f; // The height to lift the player above the map
 
+    public float respawnDuration = 2.0f; // The time it takes to rotate 180 degrees
 
+    private bool isRotating = false;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Water")
         {
-            RespawnPlayer();
+            StartCoroutine(RespawningPlayer());
+            //RespawnPlayer();
         }
     }
 
@@ -25,6 +30,7 @@ public class Respawn : MonoBehaviour
         if (other.tag == "RespawnCollider")
         {
             respawnPoint = gameObject.transform.position;
+            initialRotation = transform.rotation;
         }
     }
 
@@ -36,7 +42,33 @@ public class Respawn : MonoBehaviour
         //Debug.Log(respawnPoint + " respawn");
 
         //teleport player back onto land
-        gameObject.transform.position = respawnPoint;
+        //gameObject.transform.position = respawnPoint;
 
+        
+
+    }
+
+    private IEnumerator RespawningPlayer()
+    {
+        isRotating = true;
+        Quaternion endRotation = initialRotation * Quaternion.Euler(0, 180, 0); // Rotate 180 degrees from initial rotation
+
+        float elapsedTime = 0;
+        Vector3 initialPosition = transform.position;
+        Vector3 targetPosition = respawnPoint + Vector3.up * liftHeight; // Lift the player
+
+
+        while (elapsedTime < respawnDuration)
+        {
+            transform.rotation = Quaternion.Slerp(initialRotation, endRotation, elapsedTime / respawnDuration);
+            transform.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / respawnDuration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+        transform.position = targetPosition;
+        isRotating = false;
     }
 }
