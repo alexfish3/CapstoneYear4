5ccf2e7b51f0200313e7d1dbe0c249ed184fe6a1
LoganM5 <loganmmoss@gmail.com>
2023-11-12 19:13:38 -0500

    platforms can now wait at the end points

--- Assets/Scripts/OUTDATED/MovingPlatform.cs
+++ Assets/Scripts/OUTDATED/MovingPlatform.cs
@@ -12,21 +12,37 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] private float speed = 1.0f;
     private Transform temp = null;
 
+    [SerializeField]
+    private bool waitAtEnd = false;
+
+    [SerializeField]
+    private float secondsToWaitFor = 1;
 
     private void Start()
     {
         gameObject.transform.position = startingPos.position;
+        StartCoroutine(PauseAtEndPoint());
     }
 
-    private void FixedUpdate()
+    private IEnumerator PauseAtEndPoint()
     {
-        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, endPos.position, speed * Time.deltaTime);
-        
-        if (transform.position == endPos.position)
+        while (true)
         {
-            temp = startingPos;
-            startingPos = endPos;
-            endPos = temp;
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, endPos.position, speed * Time.deltaTime);
+
+            if (transform.position == endPos.position)
+            {
+                temp = startingPos;
+                startingPos = endPos;
+                endPos = temp;
+
+                if (waitAtEnd)
+                {
+                    yield return new WaitForSecondsRealtime(secondsToWaitFor);
+                }
+            }
+
+            yield return null;
         }
 
     }
