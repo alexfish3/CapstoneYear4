949bed14a5e435b98768baaabb80e877c6cdcef9
Alex <alexander052702@gmail.com>
2023-11-24 20:35:58 -0500

    Added basic cinemachine

--- Assets/Rendering/Shaders/HLSL/CustomLighting.hlsl
+++ Assets/Rendering/Shaders/HLSL/CustomLighting.hlsl
@@ -1,7 +1,5 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
-#pragma multi_compile _ _MAIN_LIGHT_SHADOWS
-#pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
 
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
 {
@@ -21,7 +19,9 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
     Direction = mainLight.direction;
     Color = mainLight.color;
     DistanceAtten = mainLight.distanceAttenuation;
-    ShadowAtten = mainLight.shadowAttenuation;
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowStrength = GetMainLightShadowStrength();
+    ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_LinearClampCompare), shadowSamplingData, shadowStrength, false); 
 #endif
 }
 
@@ -43,29 +43,9 @@ void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out h
     Direction = mainLight.direction;
     Color = mainLight.color;
     DistanceAtten = mainLight.distanceAttenuation;
-    ShadowAtten = mainLight.shadowAttenuation;
-#endif
-}
-
-void GetLighting(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
-{
-#if SHADERGRAPH_PREVIEW
-    Direction = half3(0.5, 0.5, 0);
-    Color = 1;
-    DistanceAtten = 1;
-    ShadowAtten = 1;
-#else
-#if SHADOWS_SCREEN
-    half4 clipPos = TransformWorldToHClip(WorldPos);
-    half4 shadowCoord = ComputeScreenPos(clipPos);
-#else
-    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-#endif
-    Light mainLight = GetMainLight(shadowCoord);
-    Direction = mainLight.direction;
-    Color = mainLight.color;
-    DistanceAtten = mainLight.distanceAttenuation;
-    ShadowAtten = mainLight.shadowAttenuation;
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowStrength = GetMainLightShadowStrength();
+    ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false); 
 #endif
 }
 
@@ -90,18 +70,6 @@ void DirectSpecular_half(half3 Specular, half Smoothness, half3 Direction, half3
     WorldNormal = normalize(WorldNormal);
     WorldView = SafeNormalize(WorldView);
     Out = LightingSpecular(Color, Direction, WorldNormal, WorldView,half4(Specular, 0), Smoothness);
-#endif
-}
-
-void GetSpecular(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
-{
-#if SHADERGRAPH_PREVIEW
-    Out = 0;
-#else
-    Smoothness = exp2(10 * Smoothness + 1);
-    WorldNormal = normalize(WorldNormal);
-    WorldView = SafeNormalize(WorldView);
-    Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, half4(Specular, 0), Smoothness);
 #endif
 }
 
