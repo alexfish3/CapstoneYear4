145348794539a614628a45c9f187a6f6aecf7778
Alex <alexander052702@gmail.com>
2024-01-10 20:58:09 -0500

    implementation of rough orbital camera

---
+++ Assets/Scripts/Player/OrbitalCamera.cs
@@ -0,0 +1,33 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalCamera : MonoBehaviour
+{
+    [SerializeField] InputManager inputManager;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+    [SerializeField] CinemachineOrbitalTransposer orb;
+    [SerializeField] float maxAngle = 180;
+    [SerializeField] float smoothSpeedValue = 0.1f;
+    [SerializeField] float realAxis;
+    [SerializeField] float smoothAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orb = virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        realAxis = RangeMutations.Map_Linear(inputManager.RightStickValue, -1, 1, -maxAngle, maxAngle);
+
+        smoothAxis = Mathf.Lerp(smoothAxis, realAxis, smoothSpeedValue);
+
+        orb.m_XAxis.Value = smoothAxis;
+        
+
+    }
+}
