6bd73c7a4430da56a1519367e51e6f9f69962043
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2024-02-06 21:39:14 -0500

    civilians be walkin

--- Assets/Scripts/World/CivilianAgent.cs
+++ Assets/Scripts/World/CivilianAgent.cs
@@ -1,8 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class CivilianAgent : MonoBehaviour
 {
-    
+    private NavMeshAgent agent;
+    private float wayPointDistance = 2f;
+
+    [SerializeField]
+    private Transform[] points;
+    public Transform[] Points { set { points = value; } }
+
+    private int currentIntendedPoint = 0;
+
+    private float slowUpdateTickSpeed = 0.1f; //How frequently, in seconds, SlowUpdate runs
+    private IEnumerator slowUpdateCoroutine;
+
+    /// <summary>
+    /// Standard Start, just gets a reference
+    /// </summary>
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        BeginPathing();
+    }
+
+    /// <summary>
+    /// Can be called externally to begin a pathing routine. Checks that points are loaded, then begins SlowUpdate
+    /// </summary>
+    public void BeginPathing()
+    {
+        if (points == null)
+        {
+            Debug.LogError(this.gameObject.name + " has no assigned patrol points.");
+            return;
+        }
+
+        StartSlowUpdate();
+    }
+
+    /// <summary>
+    /// Runs a reduced-frequency Update.
+    /// </summary>
+    /// <returns>Boilerplate IEnumerator</returns>
+    private IEnumerator SlowUpdate()
+    {
+        while (true)
+        {
+            if ((agent.destination - agent.transform.position).magnitude <= wayPointDistance)
+            {
+                CyclePoints();
+            }
+
+            yield return new WaitForSeconds(slowUpdateTickSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Cycles through the set of points, resetting the count if it reaches the end
+    /// </summary>
+    private void CyclePoints()
+    {
+        currentIntendedPoint++;
+
+        if (currentIntendedPoint >= points.Length)
+            currentIntendedPoint = 0;
+
+        agent.SetDestination(points[currentIntendedPoint].position);
+    }
+
+
+    private void StartSlowUpdate()
+    {
+        slowUpdateCoroutine = SlowUpdate();
+        StartCoroutine(slowUpdateCoroutine);
+    }
+    private void StopSlowUpdate()
+    {
+        if (slowUpdateCoroutine != null)
+        {
+            StopCoroutine(slowUpdateCoroutine);
+            slowUpdateCoroutine = null;
+        }
+    }
 }
