53199c8771e7b6d139b58d75c4f39f5af6722cc8
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2024-03-27 20:30:57 -0400

    think that fixes

--- Assets/Scripts/Player/BallDriving.cs
+++ Assets/Scripts/Player/BallDriving.cs
@@ -1049,6 +1049,7 @@ public class BallDriving : MonoBehaviour
         StopEndBoost();
         phaseIndicator.SetHornColor(1);
         boostAble = true;
+        boosting = false;
     }
 
     /// <summary>
