e159ab1a0e1269d1ca5262178c5a507197ab11e8
Alex <alexander052702@gmail.com>
2024-04-15 22:50:14 -0400

    Added some button graphics

--- Assets/Scripts/Menu/CreditsMenu.cs
+++ Assets/Scripts/Menu/CreditsMenu.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class CreditsMenu : SingletonMonobehaviour<CreditsMenu>
 {
+    [SerializeField] GameObject returnButton;
     [SerializeField] MainMenu menu;
     [SerializeField] Animator animator;
+    bool finished = false;
+
+    public void Update()
+    {
+        // Calls when the credits finished
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9 && animator.GetCurrentAnimatorStateInfo(0).IsName("Credit_Anim") && finished == false)
+        {
+            finished = true;
+            returnButton.SetActive(true);
+        }
+    }
 
     public void BeginCredits()
     {
+        returnButton.SetActive(false);
+        finished = false;
         animator.SetTrigger(HashReference._scrollCreditsTrigger);
     }
 
